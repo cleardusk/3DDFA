33f4fcbea39be2a7c4c201543c792138a1ed010a
cleardusk <guojianzhu1994@foxmail.com>
2018-10-04 22:52:37 +0800

    Refine center analysis

---
+++ visualize/render_face_mesh.m
@@ -0,0 +1,18 @@
+function render_face_mesh(vertex, tri)
+    trisurf(tri', vertex(1, :), vertex(2, :), vertex(3, :), ones(size(vertex, 2),1), 'edgecolor', 'none');
+    
+    re=[1 1 1];
+    colormap(re)
+
+    light('Position', [0 0 1], 'Style', 'infinite');
+    lighting gouraud
+    axis equal
+    view([0 90]);
+    
+    xlabel('x');
+    ylabel('y');
+    zlabel('z');
+    
+    axis on;
+    grid on;
+end
