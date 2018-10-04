1dcf61c484babc97b74fc29b6753057601993146
cleardusk <guojianzhu1994@foxmail.com>
2018-10-04 23:08:01 +0800

    Add matlab render demo

--- visualize/render_demo.m
+++ visualize/render_demo.m
@@ -4,3 +4,7 @@ vertex = load('image00427');
 tri = tri.tri;
 vertex = vertex.vertex;
 render_face_mesh(vertex, tri);
+
+A = getframe(gcf);
+mimg = A.cdata;
+imwrite(mimg, 'demo.jpg', 'quality', 95);
