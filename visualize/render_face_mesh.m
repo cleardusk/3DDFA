cb8f72229f2a9746759f0717b708af659824076c
cleardusk <guojianzhu1994@foxmail.com>
2018-10-04 22:53:19 +0800

    Add matlab render demo

--- visualize/render_face_mesh.m
+++ visualize/render_face_mesh.m
@@ -2,7 +2,7 @@ function render_face_mesh(vertex, tri)
     trisurf(tri', vertex(1, :), vertex(2, :), vertex(3, :), ones(size(vertex, 2),1), 'edgecolor', 'none');
     
     re=[1 1 1];
-    colormap(re)
+    colormap(re);
 
     light('Position', [0 0 1], 'Style', 'infinite');
     lighting gouraud
