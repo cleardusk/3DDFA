d4f758e5a81fcf91f37f5b5f1c2274ad507b7a45
cleardusk <guojianzhu1994@foxmail.com>
2018-12-27 19:22:51 +0800

    Fix PNCC

--- utils/params.py
+++ utils/params.py
@@ -38,3 +38,6 @@ paf = _load(osp.join(d, 'Model_PAF.pkl'))
 u_filter = paf.get('mu_filter')
 w_filter = paf.get('w_filter')
 w_exp_filter = paf.get('w_exp_filter')
+
+# pncc code (mean shape)
+pncc_code = _load(osp.join(d, 'pncc_code.npy'))
